c9cce4f51a9b229118cbeb484658e4d76b20b590
Bohou007 <bohoussoupaulelie@gmail.com>
2022-10-07 15:55:37 +0000

    Valid project 3

--- run-all-script.sh
+++ run-all-script.sh
@@ -2,8 +2,8 @@ chmod +x env-script.sh
 ./env-script.sh
 
 
-chmod +x service-script.sh
-./service-script.sh
-
 chmod +x deployment-script.sh
 ./deployment-script.sh
+
+chmod +x service-script.sh
+./service-script.sh
--- service-script.sh
+++ service-script.sh
@@ -3,6 +3,8 @@ kubectl delete svc backend-user
 kubectl delete svc reverseproxy
 kubectl delete svc frontend
 
+# kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+# kubectl get deployment metrics-server -n kube-system
 
 kubectl apply -f feed-services.yml
 kubectl apply -f user-services.yml
